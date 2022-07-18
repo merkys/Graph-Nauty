328f8f991b1478cad3e8e05a7edd2fbebc843f0f
Andrius Merkys <andrius.merkys@gmail.com>
2022-07-18 12:02:22 +0300

    Adjusting copyright footer to mention BSD-3-Clause.

--- lib/Graph/Nauty.pm
+++ lib/Graph/Nauty.pm
@@ -326,8 +326,6 @@ Andrius Merkys, L<mailto:merkys@cpan.org>
 
 Copyright (C) 2020 by Andrius Merkys
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.26.1 or,
-at your option, any later version of Perl 5 you may have available.
+Graph::Nauty is distributed under the BSD-3-Clause license.
 
 =cut
